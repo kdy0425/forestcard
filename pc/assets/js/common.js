7f1727dfa2402acf982435ad8771ef65f6f29c3c
DongYoung Kim <kdy042525@gmail.com>
2024-08-06 23:48:22 +0900

    dd

--- pc/assets/js/common.js
+++ pc/assets/js/common.js
@@ -452,3 +452,24 @@ if(searchSelects){
 	
 }
 });
+
+//데이트픽커
+if ($('input').hasClass('datepicker2')) {
+	$.datepicker.setDefaults({
+		dateFormat: 'yy-mm-dd',
+		prevText: '이전 달',
+		nextText: '다음 달',
+		monthNames: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+		monthNamesShort: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+		changeMonth: true, //셀렉트박스로 월선택
+		changeYear: true, //셀렉트박스로 년선택
+		showMonthAfterYear: true,
+		//showOn: 'button',
+		yearSuffix: ' -'
+	});
+
+	$(".datepicker2").datepicker({});
+}
